f34c9224761f5fb4635e8848547b12fea8f77767
virtustron <virtustron@gmail.com>
2021-09-28 20:18:16 +0300

    Update include path for *.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 
 int main(int argc, char *argv[])
 {
-	//std::cout << "Hello Makefile!\n";
-	std::cout << "special edit for gitignore testing\n";
-
 	ChessBoardConsoleUI chessbouard_console_ui;
 	chessbouard_console_ui.StartChessBoardDialog(argc, argv);
 
